79bf6a6219299b9d93edd36083546afa40aa0e8f
enriqueFcoG <enrikefrancisco@gmail.com>
2018-12-21 08:13:19 -0800

    pantalla de login terminada

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,38 +1,159 @@
-/**
- * PANTALLA DE LOGINA
- */
-
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
-
-
-export default class App extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
-    );
+import {Platform, 
+        StyleSheet, 
+        Text, 
+        View, 
+        Button, 
+        TextInput,
+        Image, 
+        StatusBar, 
+        TouchableHighlight, ScrollView} from 'react-native';
+import {createStackNavigator, createAppContainer} from 'react-navigation';
+import Signup from './signup';
+class HomeScreen extends Component {
+constructor(){
+  super()
+  this.state = {
+    contador : 0,
+    usuario: "",
+    clave: "",
+    mensaje: '',
+    errorMessage: ''
   }
+ 
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+
+  static navigationOptions = {
+    title: 'Welcome',
+    header: null
+  };
+    render() {
+      return (        
+        <View style={{backgroundColor:"#212121", flex:1}}>
+        <ScrollView style={styles.estiloScroll}>
+        
+        <StatusBar
+        backgroundColor="#212121"
+        barStyle="light-content"/>
+        
+        <View style={{alignItems: 'center', justifyContent: 'center',height:150, backgroundColor:"#212121"}}>
+        <Text style={styles.tituloFLET}>FLET</Text>
+        </View>
+        <View style={styles.container}>
+        <TextInput style={styles.textosLogin}           
+          underlineColorAndroid='#4ccfe0'
+          placeholder="correo"
+          placeholderTextColor='#4ccfe0'
+        />
+          
+        <TextInput           
+          style={styles.textosLogin} 
+          placeholderTextColor='#4ccfe0'
+          underlineColorAndroid="#4ccfe0" 
+          placeholder="contraseña"
+          />
+          <View style={styles.btnLogin}>
+            <Button 
+              color= '#4ccfe0'
+              title="Log In"
+              
+              onPress={(ingresar)}
+            />  
+          </View>
+        <Text style={styles.forgotText} 
+          onPress={(ingresar)}>¿Olvidaste tu contraseña?
+        </Text>  
+
+         
+          <Text style={[styles.forgotText, this.state.colorTexto]} 
+          onPress={()=> this.props.navigation.navigate('Registro')}>¿Nuevo usuario? Registrate
+        </Text> 
+        </View>
+        </ScrollView>        
+      </View>
+
+      
+      );
+    }
+  }
+
+
+  const ingresar = () =>{
+    alert("faste");
+  }
+
+  const styles = StyleSheet.create({
+    container: {
+      alignItems: 'center',
+      backgroundColor: '#212121'
+    },
+    estiloScroll: {
+      marginRight: 0,
+      marginLeft: 0,
+      marginBottom: 0
+    },
+   tituloFLET: {
+     fontSize: 70,
+     color:"#FFFFFF"
+   },
+    welcome: {
+      fontSize: 30,
+      textAlign: 'center',
+      margin: 10,
+      color: '#83C587'
+    },
+    instructions: {
+      textAlign: 'center',
+      color: '#333333',
+      marginBottom: 5,
+      marginTop:0,
+    },
+    logo: {
+      width: 150,
+      height: 150,
+    },
+    textosLogin: {
+      width: 300,
+      color: '#83C587',
+      marginTop: 20,
+      
+    },
+    forgotText: {
+      fontSize:12,
+      textAlign: 'center',
+      marginTop: 0,
+      color: '#c2c2c2',
+    },
+    textoOR:{
+      fontSize: 15,
+      textAlign: 'center',
+      color: '#83C587'
+    },
+    btnLogin: {
+      width: 300,
+      color:"white",
+      height: 50,
+      marginTop: 50,
+    },
+    btnFacebook: {
+      width: 300,
+      height: 80,
+      marginTop: 10
+    }
+  });
+  
+  
+  const RootStack = createStackNavigator(
+    {
+      Home: HomeScreen,
+      Registro: Signup,
+
+    },{initialRouteName: 'Home',},
+  );
+
+  const Login = createAppContainer(RootStack);
+  
+  export default Login;
+
+
--- src/screens/signup.js
+++ src/screens/signup.js
@@ -1,5 +1,5 @@
 /**
- * PANTALLA DE LOGINA
+ * PANTALLA DE REGISTRO
  */
 
 import React, {Component} from 'react';
@@ -12,7 +12,7 @@ export default class App extends Component {
       <View style={styles.container}>
         <Text style={styles.welcome}>Welcome to React Native!</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        
       </View>
     );
   }
